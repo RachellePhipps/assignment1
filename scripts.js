1d25994abb757e5387f6c8fa6dd0aa9a0d6f6b44
Rachelle Phipps <rachelle.s.phipps@ou.edu>
2022-09-29 12:24:59 -0500

    tweets isn't working

--- scripts.js
+++ scripts.js
@@ -4,67 +4,14 @@ $(function() {
   $('#get-button').on('click', function() {
        //TODO: get all users' IDs & display it
         console.log('this is the get-button');
-
+      
+        $.ajax({
+          url: '/tweets',
+          contentType: 'application/json',
+          success: function(response) {
+            console.log(response);
+          }
+        });
    });
-
-
-   //Get tweets
-   $('#get-tweets-button').on('click', function(){
-       //TODO: get tweet info and display it
-
-       console.log('this is the get-tweets-button');
-
-   });
-
-   //Get searched tweets
-   $('#get-searched-tweets').on('click', function() {
-       //TODO: get a searched tweet(s) & display it
-       console.log('this is the get-searched-tweets button');
-   });
-
-/*
- //CREATE
- $('#create-form').on('submit', function(event){
-       event.preventDefault();
-
-       var createInput = $('#create-input');
-
-       //TODO: creat a tweet
- });
-
-   //Create searched tweets
- $('#search-form').on('submit', function(event){
-   event.preventDefault();
-   var userID = $('#search-input');
-   
-   //TODO: search a tweet and display it.
-
- });
-
- //UPDATE/PUT
- $("#update-user").on('submit', function(event){
-     event.preventDefault();
-   var updateInput = $('#update-input');
-   var inputString = updateInput.val();
-
-   const parsedStrings = inputString.split(';');
-
-   var name = parsedStrings[0];
-   var newName = parsedStrings[1];
-   
-   //TODO: update a tweet
-
- });
-
-
- //DELETE
- $("#delete-form").on('submit', function() {
-   var id = $('#delete-input')
-   event.preventDefault();
-
-   //TODO: delete a tweet
-
- });
-*/
 });
 
