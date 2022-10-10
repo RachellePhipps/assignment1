ad562b7783dfc5edb22b143f69ff3aa156d6b978
Rachelle Phipps <rachelle.s.phipps@ou.edu>
2022-10-10 10:15:45 -0500

    trying to merge again

--- scripts.js
+++ scripts.js
@@ -202,13 +202,10 @@ $(function() {
       });
  });
 });
-<<<<<<< Updated upstream
                    
-=======
       
 function test_print(){
-  console.log(“test code”)
+  console.log(“test code”),
 
 }
->>>>>>> Stashed changes
   
